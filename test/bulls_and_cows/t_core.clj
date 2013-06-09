ec770d7cb239beeb6011f10ddac1425825a98072
Andy Marks <amarks@thoughtworks.com>
2013-06-09 23:18:54 +1000

    Now running tests multiple times for + confidence

--- test/bulls_and_cows/t_core.clj
+++ test/bulls_and_cows/t_core.clj
@@ -3,19 +3,22 @@
   (:require [bulls_and_cows.core :as core]))
 
 (facts "the rules of bulls and cows"
-       (fact "the length of the number should be 4"
-             (let [number (core/generate-number)]
-               (count number) => 4))
-       (fact "the number should be entirely numeric"
-             (let [number (core/generate-number)]
-               (filterv #(re-find #"[0-9]" (str %)) number) => number
-               ))
-       (fact "the number should contain unique digits"
-             (let [number (core/generate-number)]
-               (count (set number)) => (count number)
-               ))
-       (fact "the number should vary from call to call"
-             (let [number1 (core/generate-number)
-                   number2 (core/generate-number)]
-               (= number1 number2) => false))
-       )
+       (facts "when generating numbers"
+              (facts "for each number generated"
+                     (dotimes [_ 100]
+                       (fact "the length of the number should be 4"
+                             (let [number (core/generate-number)]
+                               (count number) => 4))
+                       (fact "the number should be entirely numeric"
+                             (let [number (core/generate-number)]
+                               (filterv #(re-find #"[0-9]" (str %)) number) => number
+                               ))
+                       (fact "the number should contain unique digits"
+                             (let [number (core/generate-number)]
+                               (count (set number)) => (count number)
+                               ))
+                       (fact "the number should vary from call to call"
+                             (let [number1 (core/generate-number)
+                                   number2 (core/generate-number)]
+                               (= number1 number2) => false))
+                       ))))
