abb42acd42def02ec7cfe209d0b30130298a7741
Andy Marks <amarks@thoughtworks.com>
2013-07-14 22:12:49 +1000

    Added session storage for answer plus form and routes to supply answer and use it in evaluating guess.  Still need to display results of evaluation

--- src/bulls_and_cows/routes/home.clj
+++ src/bulls_and_cows/routes/home.clj
@@ -3,8 +3,8 @@
   (:require [bulls-and-cows.views.layout :as layout]
             [bulls-and-cows.views :as views]
             [ring.util.response :as response]
-            [taoensso.timbre :as timbre
-             :refer (trace debug info warn error fatal spy)]
+            [taoensso.timbre :as timbre :refer (trace debug info warn error fatal spy)]
+            [noir.session :as session]
             [bulls-and-cows.model.core :as core]
             [bulls-and-cows.util :as util]))
 
@@ -18,13 +18,22 @@
 (defn start-page []
   (layout/render "header.html" {:content (views/guess-form)}))
 
+(defn enter-answer []
+  (layout/render "header.html" {:content (views/answer-form)}))
+  
 (defn evaluate-guess [guess]
-  (info "Guess: " guess ", answer: " (core/guess-number '(1 2 3 4) guess))
+  (let [answer (map read-string (rest (clojure.string/split (session/get :answer) #"")))]
+    (info "Guess: " guess ", answer: (" answer ") " (core/guess-number answer guess)))
   (response/redirect "/start"))
  
-  
+(defn record-answer [answer]
+  (session/put! :answer answer)
+  (response/redirect "/start"))
+
 (defroutes home-routes
   (GET "/" [] (home-page))
   (POST "/submit-guess" [guess] (evaluate-guess guess))
+  (GET "/admin/answer" [] (enter-answer))
+  (POST "/submit-answer" [answer] (record-answer answer))
   (GET "/start" [] (start-page))
   (GET "/about" [] (about-page)))
--- src/bulls_and_cows/views.clj
+++ src/bulls_and_cows/views.clj
@@ -6,3 +6,9 @@
    (form-to [:post "/submit-guess"]
             (text-field :guess)
             (submit-button "Try this!"))))
+
+(defn answer-form []
+  (html5
+   (form-to [:post "/submit-answer"]
+            (text-field :answer)
+            (submit-button "Enter answer"))))
